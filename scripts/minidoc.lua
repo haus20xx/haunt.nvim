ed48d5e54322e4b35ed1af042a15c11a04e9f7e6
Noe Trevino <noetrevino.music@gmail.com>
2026-01-18 11:01:20 +0000

    docs: add docs generator

---
+++ scripts/minidoc.lua
@@ -0,0 +1,16 @@
+-- run with: :lua MiniDoc.generate() or :luafile scripts/minidoc.lua
+
+local MiniDoc = require("mini.doc")
+
+-- define order for the docs
+local files = {
+	"lua/haunt/init.lua", -- Main module, introduction, TOC
+	"lua/haunt/config.lua", -- Configuration options
+	"lua/haunt/api.lua", -- Public API functions
+	"lua/haunt/persistence.lua", -- Bookmark structure
+	"lua/haunt/picker.lua", -- Picker integration
+	"lua/haunt/sidekick.lua", -- Sidekick integration
+	"plugin/haunt.lua", -- Commands
+}
+
+MiniDoc.generate(files, "doc/haunt.txt")
